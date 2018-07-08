a9221efb402f89ff7db3d74cec37d03a7c8d8bb0
fadli mardian <fadli.mardian@surocode.com>
2018-07-08 16:28:13 +0700

    Updates Tab Page. Add Table View and table view cell

---
+++ iqro/iqro/Views/UpdateTableViewCell/UpdatesTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  UpdatesTableViewCell.swift
+//  iqro
+//
+//  Created by admin on 7/8/18.
+//  Copyright © 2018 MyMacbook. All rights reserved.
+//
+
+import UIKit
+
+class UpdatesTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
