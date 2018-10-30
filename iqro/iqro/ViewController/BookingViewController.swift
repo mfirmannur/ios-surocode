0f883ac82bdd78c34a70c8b3c6bdd3a090123136
Ardy Joan Pradito <fadli.mardian@surocode.com>
2018-10-30 22:21:50 +0700

    Fix booking UI

--- iqro/iqro/ViewController/BookingViewController.swift
+++ iqro/iqro/ViewController/BookingViewController.swift
@@ -10,27 +10,20 @@ import UIKit
 
 class BookingViewController: UIViewController {
     
+    @IBOutlet weak var bookFrontView: UIView!
+    @IBOutlet weak var bookRearView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let bookFrontTapped = UITapGestureRecognizer(target: self, action: #selector(self.bookFrontClicked))
+        bookFrontView.addGestureRecognizer(bookFrontTapped)
+        bookFrontView.isUserInteractionEnabled = true
         // Do any additional setup after loading the view.
         self.navigationController?.navigationBar.isHidden = true
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @objc func bookFrontClicked() {
+        
     }
-    */
 
 }
